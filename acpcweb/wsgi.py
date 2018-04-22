8b3a0d46548ef0c092ec6f083d9d888c2f46fe38
Eleonor Vinicius <Vinicius Mayer>
2018-04-22 12:57:37 -0300

    Start project

---
+++ acpcweb/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for acpcweb project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "acpcweb.settings")
+
+application = get_wsgi_application()
