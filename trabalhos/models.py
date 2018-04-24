0322a1bb16bc9cb0aa6457692a7436cafeee7c3b
Vinicius Mayer <viniciusmayer@gmail.com>
2018-04-24 18:55:13 -0300

    Iniciando modelagem de dados do projeto

--- trabalhos/models.py
+++ trabalhos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Arquivo(models.Model):
+    nome = models.CharField(editable=False, unique=True, max_length=255)
+        
+class Trabalho(models.Model):
+    ano = models.PositiveSmallIntegerField(editable=False)
+    titulo = models.CharField(editable=False, max_length=255)
+    natureza = models.CharField(editable=False, max_length=255)
+    arquivo = models.ForeignKey(Arquivo, on_delete=models.SET_NULL, null=True)
