7825c2f067bb32b146852cbad029b3efd7dbd584
Vinicius Mayer <viniciusmayer@gmail.com>
2018-06-13 09:58:49 -0300

    Pequenos ajustes para melhor visualização dos dados

--- trabalhos/admin.py
+++ trabalhos/admin.py
@@ -3,12 +3,12 @@ from trabalhos.models import Trabalho, Arquivo
 
 
 class TrabalhoAdmin(admin.ModelAdmin):
-    list_display = ('ano', 'titulo', 'natureza',)
+    list_display = ('ano', 'titulo', 'natureza', 'arquivo',)
     list_filter = ['ano', 'natureza',]
     search_fields = ['titulo',]
 admin.site.register(Trabalho, TrabalhoAdmin)
 
 class ArquivoAdmin(admin.ModelAdmin):
-    list_display = ('arquivo', 'paginas')
+    list_display = ('nomeArquivo', 'paginas')
     search_fields = ['arquivo',]
 admin.site.register(Arquivo, ArquivoAdmin)
--- trabalhos/models.py
+++ trabalhos/models.py
@@ -4,7 +4,13 @@ from django.db import models
 class Arquivo(models.Model):
     arquivo = models.FileField(max_length=255, unique=True)
     paginas = models.PositiveSmallIntegerField()
-        
+
+    def __str__(self):
+        return '{0}'.format(self.nomeArquivo())
+    
+    def nomeArquivo(self):
+        return self.arquivo.name[self.arquivo.name.rfind('/') + 1:len(self.arquivo.name)]
+
 class Trabalho(models.Model):
     ano = models.PositiveSmallIntegerField()
     titulo = models.CharField(max_length=255)
@@ -13,6 +19,9 @@ class Trabalho(models.Model):
 
     class Meta:
         unique_together = ('ano', 'titulo', 'natureza')
+        
+    def __str__(self):
+        return '{0}, {1}, {2}'.format(self.ano, self.titulo, self.natureza)
 
 class Evento(models.Model):
     nome = models.CharField(max_length=255)
@@ -21,7 +30,10 @@ class Evento(models.Model):
     
     class Meta:
         unique_together = ('nome', 'quando')
-    
+
+    def __str__(self):
+        return '{0}, {1}, {2}'.format(self.quando, self.nome)
+
 class EventoTrabalho(models.Model):
     evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
     trabalho = models.ForeignKey(Trabalho, on_delete=models.CASCADE)
@@ -29,3 +41,6 @@ class EventoTrabalho(models.Model):
 
     class Meta:
         unique_together = ('evento', 'trabalho', 'ordem')
+
+    #def __str__(self):
+    #    return '{0}, {1}, {2}'.format(self.quando, self.nome)
