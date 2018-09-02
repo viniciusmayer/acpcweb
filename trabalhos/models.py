12bdefdb84d05ac4a980a31663b6467035fda319
Vinicius Mayer <viniciusmayer@gmail.com>
2018-09-01 21:04:13 -0300

    Inclusão de ordenação nas entidades.

--- trabalhos/models.py
+++ trabalhos/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+
 class Entidade(models.Model):
     nome = models.CharField(max_length=255, unique=True)
     descricao = models.CharField(max_length=255)
     
+    class Meta:
+        ordering = ['nome']
+
     def __str__(self):
         return '{s}'.format(s=self.nome)
     
@@ -11,6 +15,9 @@ class Entidade(models.Model):
 class Arquivo(models.Model):
     arquivo = models.FileField(max_length=255, unique=True)
     paginas = models.PositiveSmallIntegerField()
+
+    class Meta:
+        ordering = ['arquivo']
 
     def __str__(self):
         return '{0}'.format(self.nomeArquivo())
@@ -24,6 +31,9 @@ class Tag(models.Model):
     descricao = models.CharField(max_length=255)
     ordem = models.PositiveSmallIntegerField(unique=True)
     
+    class Meta:
+        ordering = ['nome']
+    
     def __str__(self):
         return '{0}'.format(self.nome)
 
@@ -32,21 +42,25 @@ class Natureza(models.Model):
     nome = models.CharField(max_length=255, unique=True)
     descricao = models.CharField(max_length=255)
     
+    class Meta:
+        ordering = ['nome']
+    
     def __str__(self):
         return '{0}'.format(self.nome)
 
 
 class Trabalho(models.Model):
     ano = models.PositiveSmallIntegerField()
-    ano_fim = models.PositiveSmallIntegerField(null=True)
+    ano_fim = models.PositiveSmallIntegerField(null=True, blank=True)
     titulo = models.CharField(max_length=255)
     tag = models.ForeignKey(Tag, on_delete=models.PROTECT)
-    natureza = models.ForeignKey(Natureza, on_delete=models.SET_NULL, null=True)
-    arquivo = models.ForeignKey(Arquivo, on_delete=models.SET_NULL, null=True)
-    entidade = models.ForeignKey(Entidade, on_delete=models.SET_NULL, null=True)
+    natureza = models.ForeignKey(Natureza, on_delete=models.SET_NULL, null=True, blank=True)
+    arquivo = models.ForeignKey(Arquivo, on_delete=models.SET_NULL, null=True, blank=True)
+    entidade = models.ForeignKey(Entidade, on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         unique_together = ('ano', 'titulo', 'natureza', 'tag')
+        ordering = ['titulo']
 
     def __str__(self):
         return '{0}, {1}, {2}'.format(self.ano, self.nomeTrabalho(), self.tag)
@@ -58,6 +72,7 @@ class Trabalho(models.Model):
             nomeTrabalho = nomeTrabalho[0:i]
         return '{0}...'.format(nomeTrabalho if len(nomeTrabalho) < 65 else nomeTrabalho[0:64])
 
+
 class Evento(models.Model):
     nome = models.CharField(max_length=255)
     quando = models.DateField()
@@ -65,6 +80,7 @@ class Evento(models.Model):
     
     class Meta:
         unique_together = ('nome', 'quando')
+        ordering = ['nome']
 
     def __str__(self):
         return '{0}'.format(self.nome)
